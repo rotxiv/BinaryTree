1cdab9336d670b2e265a395a93e684acca32d69b
rotxiv <rotxiv0@gmail.com>
2023-10-02 23:05:10 -0400

    Funciones agregadas a BinaryTree

--- BinaryTree.py
+++ BinaryTree.py
@@ -8,11 +8,14 @@ class BinaryTree(object):
 	def isEmptyTree(self):
 		return BinaryNode.isEmptyNode(self.root)
 
+	def clear(self):
+		self.root = None
+
 	def insert(self, key, value):
 		if BinaryNode.isEmptyNode(self.root):
 			self.root = BinaryNode(key, value)
 		else:
-			self.__recursiveInsert(node, key, value)
+			self.__recursiveInsert(self.root, key, value)
 
 	def __recursiveInsert(self, node, key, value):
 		if key < node.key:
@@ -27,11 +30,51 @@ class BinaryTree(object):
 				self.__recursiveInsert(node.rightSon, key, value)
 
 	def search(self):
-		
+		pass	
+	
+	def inOrden(self):
+		listInOrden = []
+		self.__inOrden(self.root, listInOrden)
+		return listInOrden
+
+	def __inOrden(self, node, listInOrden):
+		if not BinaryNode.isEmptyNode(node):
+			self.__inOrden(node.leftSon, listInOrden)
+			listInOrden.append(node.key)
+			self.__inOrden(node.rightSon, listInOrden)
+
+	def posOrden(self):
+		listPosOrden = []
+		self.__posOrden(self.root, listPosOrden)
+		return listPosOrden
+
+	def __posOrden(self, node, listPosOrden):
+		if not BinaryNode.isEmptyNode(node):
+			self.__posOrden(node.leftSon, listPosOrden)
+			self.__posOrden(node.rightSon, listPosOrden)
+			listPosOrden.append(node.key)
+
+	def preOrden(self):
+		listPreOrden = []
+		self.__preOrden(self.root, listPreOrden)
+		return listPreOrden
+
+	def __preOrden(self, node, listPreOrden):
+		if not BinaryNode.isEmptyNode(node):
+			listPreOrden.append(node.key)
+			self.__preOrden(node.leftSon, listPreOrden)
+			self.__preOrden(node.rightSon, listPreOrden)
 
 
 arbol = BinaryTree()
-print(arbol.isEmptyTree())
-arbol.insert(10, "Hola Mundo...!")
-print(arbol.isEmptyTree())
-print(arbol.root.value)
+arbol.insert(10, "Santa Cruz")
+arbol.insert(20, "La Paz")
+arbol.insert(5, "Cochabamba")
+arbol.insert(7, "Tarija")
+arbol.insert(15, "Potosi")
+print("recorrido en inOrden : ")
+print(arbol.inOrden())
+print("recorrido en preOrden : ")
+print(arbol.preOrden())
+print("recorrido en posOrden : ")
+print(arbol.posOrden())
